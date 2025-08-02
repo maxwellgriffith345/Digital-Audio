e67e7cf2e72cabfde94a9bb7eeeb396d46e59da3
Maxwell Griffith <maxwellgriffith345@gmail.com>
2025-08-02 17:21:33 -0500

    cleaned up high pass

--- hp_fir.py
+++ hp_fir.py
@@ -14,11 +14,16 @@ Using Callback method
 """
 
 """
-niave finite impulse response highpass filter
+finite impulse response highpass filter
 y[n] = (0.5*x[n])-(0.5*x[n-1])
+y[n]=x[n]-x[n-1]
 implamentation based on The Computer Music Tutorial Chapter 28
 works by phase cancelation
 what frequency is the cut off?
+
+output[1]=x[1]
+output[2]=x[2]-x[1]
+output[3]=x[3]-[x2]
 """
 
 #frames/samples per second
@@ -28,12 +33,17 @@ chunk = 1024
 volume = .2
 last_sample = 0
 
+delay = 0
 
 #fill an numpy array with the data
 def noise_callback(outdata, frames, time, status):
-    noise = np.random.normal(0,volume, size=(frames+1,)).astype(np.float32)
-    hp_noise = 0.5*noise[1:]-0.5*noise[:-1]
-    outdata[:]=hp_noise.reshape(-1,1)
+    global delay
+    noise = np.random.normal(0,volume, size=(frames,)).astype(np.float32)
+    for i in range(frames):
+        next_delay = noise[i]
+        noise[i]=0.5*noise[i]+0.5*delay #but when do we update delay value?
+        delay = next_delay
+    outdata[:]=noise.reshape(-1,1)
 
 
 try:
